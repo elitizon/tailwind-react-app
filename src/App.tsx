e4f32ace50e56f4ed701129052d81cc98112fa25
Raphael MANSUY <raphael.mansuy@caliatys.com>
2020-09-01 21:35:50 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import tw from "twin.macro"
+import "./assets/main.css"
+import content   from "./logo.svg"
+
+const Header = tw.h1`text-blue-700 text-6xl`
+const StyleApp = tw.div`bg-gray-200 h-screen`
+
+function* range(x:number, y:number) {
+  while (true) {
+    if (x <= y)
+      yield x++
+    else
+      return null
+  }
+}
+/*
+l.map((x,i) => { return (
+  <Header>{x.toString()}</Header>
+)}*/
+
 
 function App() {
+  const l = Array.from(range(0,2000))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StyleApp className="App">
+      <img src={content} />
+      <Header>learn react</Header>
+    </StyleApp>
+  )
 }
 
-export default App;
+export default App
